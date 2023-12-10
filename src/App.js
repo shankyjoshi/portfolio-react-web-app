88bbd4aca556fee5fbfd34096b45e300d7ad4e6d
jf13-shreya-teammate <938091@cognizant.com>
2023-12-10 18:43:02 -0500

    public folder images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Checkout from "./components/Checkout";
+import Home from "./components/Home";
 import Intro from "./components/Portfolio/Intro";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Intro />
+        <Home />
       </BrowserRouter>
     </div>
   );
